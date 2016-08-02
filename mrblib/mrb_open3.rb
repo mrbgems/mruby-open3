62864c944c0f0717704134135de57c20f4ce8a05
Takashi Kokubun <takashikkbn@gmail.com>
2016-08-03 00:59:57 +0900

    Pass args to execvp

--- mrblib/mrb_open3.rb
+++ mrblib/mrb_open3.rb
@@ -1,6 +1,6 @@
 module Open3
   def capture3
-    p spawn("ruby", "-e", "$stdout.puts 'out'; $stderr.puts 'err'")
+    p spawn("ruby", "-e", "$stdout.puts 'out'; $stderr.puts 'err'", {})
   end
   module_function :capture3
 end
--- src/mrb_open3.c
+++ src/mrb_open3.c
@@ -8,27 +8,50 @@
 
 #include <unistd.h>
 #include "mruby.h"
-#include "mruby/value.h"
+#include "mruby/string.h"
 #include "mrb_open3.h"
 
 #define DONE mrb_gc_arena_restore(mrb, 0);
 
+static const char**
+mrb_str_buf_to_cstr_buf(mrb_state *mrb, mrb_value *strs, mrb_int num)
+{
+  const char **ret;
+  int i;
+
+  ret = (const char **)mrb_malloc(mrb, sizeof(char *) * (num+1));
+  for (i = 0; i < num; i++) {
+    ret[i] = mrb_string_value_ptr(mrb, strs[i]);
+  }
+  ret[num] = NULL;
+  return ret;
+}
+
 // `spawn` is defined under `Open3` since it's incomplete and it should be added
 // to `Process` by "mruby-process" mrbgem.
-static mrb_value mrb_open3_spawn(mrb_state *mrb, mrb_value self)
+//
+// Limitation: Currently it expects the last argument to be a Hash.
+static mrb_value
+mrb_open3_spawn(mrb_state *mrb, mrb_value self)
 {
-  mrb_value *argv;
+  const char **cmd;
+  pid_t pid;
+  mrb_value *argv, options;
   mrb_int argc;
   mrb_get_args(mrb, "*", &argv, &argc);
 
-  pid_t pid = fork();
+  cmd = mrb_str_buf_to_cstr_buf(mrb, argv, argc-1);
+  // options = argv[argc-1];
+
+  pid = fork();
   if (pid == 0) {
-    execlp("echo", "echo", "hello world", NULL);
+    execvp(cmd[0], cmd);
   }
   return mrb_fixnum_value(pid);
 }
 
-void mrb_mruby_open3_gem_init(mrb_state *mrb)
+void
+mrb_mruby_open3_gem_init(mrb_state *mrb)
 {
   struct RClass *open3;
   open3 = mrb_define_module(mrb, "Open3");
@@ -37,6 +60,7 @@ void mrb_mruby_open3_gem_init(mrb_state *mrb)
   DONE;
 }
 
-void mrb_mruby_open3_gem_final(mrb_state *mrb)
+void
+mrb_mruby_open3_gem_final(mrb_state *mrb)
 {
 }
